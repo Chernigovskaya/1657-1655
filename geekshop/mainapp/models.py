dbc569312b7190c1cd1b3768df1c681c58f4b5c4
Chernigovskaya <m.n.chernigov@gmail.com>
2022-03-29 17:50:34 +0700

    create model product and category

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+''' models for product and category '''
 
 # Create your models here.
+# ''' model category '''
+class ProductCategories(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    descriptions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+'''model for product'''
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    descriptions = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
