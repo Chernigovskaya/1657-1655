d72262ccf6c7603959a4c9565640c0128ff53384
Chernigovskaya <m.n.chernigov@gmail.com>
2022-04-23 19:55:26 +0700

    detail

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,11 @@
 import json
 
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.shortcuts import render
 import os
+
+from django.views.generic import DetailView
+
 from mainapp.models import ProductCategories, Product
 
 MODULE_DIR = os.path.dirname(__file__)
@@ -14,60 +18,31 @@ def index(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
+def products(request, id_category=None, page=1):
 
-    '''goods = [
-      {
-        'name': 'Худи черного цвета с монограммами adidas Originals',
-        'price': 6090,
-        'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-        'image': 'vendor/img/products/Adidas-hoodie.png'
-      },
-      {
-        'name': 'Синяя куртка The North Face',
-        'price': 23725,
-        'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель',
-        'image': 'vendor/img/products/Blue-jacket-The-North-Face.png'
-      },
-      {
-        'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-        'price': 3390,
-        'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-        'image': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'
-      },
-      {
-        'name': 'Черный рюкзак Nike Heritage',
-        'price': 2340,
-        'description': 'Плотная ткань. Легкий материал.',
-        'image': 'vendor/img/products/Black-Nike-Heritage-backpack.png'
-      },
-      {
-        'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-        'price': 13590,
-        'description': 'Гладкий кожаный верх. Натуральный материал.',
-        'image': 'vendor/img/products/Black-Dr-Martens-shoes.png'
-      },
-      {
-        'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-        'price': 2890,
-        'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-        'image': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'
-      }
-    ]
+    if id_category:
+        products_ = Product.objects.filter(category_id=id_category)
+    else:
+        products_ = Product.objects.all()
 
-    categories = [
-        {'name': 'Новинки'},
-        {'name': 'Одежда'},
-        {'name': 'Обувь'},
-        {'name': 'Аксессуары'},
-        {'name': 'Подарки'}
-    ]'''
+    paginator = Paginator(products_, per_page=2)
+
+    try:
+        product_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        product_paginator = paginator.page(1)
+    except EmptyPage:
+        product_paginator = paginator.page(paginator.num_pages)
 
     content = {
         'title': 'Geekshop - Каталог',
         'categories': ProductCategories.objects.all(),
-        'products': Product.objects.all()
+        'products': product_paginator
     }
+
     return render(request, 'mainapp/products.html', content)
 
 
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
