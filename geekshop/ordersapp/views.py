025dd8c93e997392595e3e110ae6df709748be2d
Chernigovskaya <88422949+Chernigovskaya@users.noreply.github.com>
2022-05-25 17:58:02 +0700

    Revert "Lesson2 6"

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,15 +1,12 @@
 from django.db import transaction
-from django.db.models.signals import pre_save, pre_delete
-from django.dispatch import receiver
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, \
     DeleteView
 
 from adminapp.mixin import BaseClassContextMixin
 from basketapp.models import Basket
-from mainapp.models import Product
 from ordersapp.forms import OrderItemsForm
 from ordersapp.models import Order, OrderItem
 
@@ -24,6 +21,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
     fields = []
     success_url = reverse_lazy('orders:list')
     title = 'Geeksop | Создание заказов'
+
 
     def get_context_data(self, **kwargs):
         context = super(OrderCreate, self).get_context_data()
@@ -42,7 +40,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                basket_item.delete()
+                #basket_item.delete()
             else:
                 formset = OrderFormSet()
 
@@ -50,6 +48,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
         return context
 
     def form_valid(self, form):
+
         context = self.get_context_data()
         orderitems = context['orderitems']
 
@@ -58,7 +57,8 @@ class OrderCreate(CreateView, BaseClassContextMixin):
             self.object = form.save()
             if orderitems.is_valid():
                 orderitems.instance = self.object
-                orderitems.save()
+            orderitems.save()
+
         if self.object.get_total_cost() == 0:
             self.object.delete()
         return super(OrderCreate, self).form_valid(form)
@@ -117,31 +117,3 @@ def order_forming_complete(request, pk):
     order.status = Order.SEND_TO_PROCESSED
     order.save()
     return HttpResponseRedirect(reverse('orders:list'))
-
-
-def get_product_price(request, pk):
-    if request.is_ajax():
-        product = Product.objects.get(pk=pk)
-        if product:
-            return JsonResponse({'price': product.price})
-        else:
-            return JsonResponse({'price': 0})
-
-
-@receiver(pre_save, sender=OrderItem)
-@receiver(pre_save, sender=Basket)
-def product_quantity_update_save(sender, instance, **kwargs):
-    if instance.pk:
-        item = instance.get_item(int(instance.pk))
-        instance.product.quantity -= instance.quantity - item
-    else:
-        instance.product.quantity -= instance.quantity
-    instance.product.save()
-
-
-@receiver(pre_delete, sender=OrderItem)
-@receiver(pre_delete, sender=Basket)
-def product_quantity_update_delete(sender, instance, **kwargs):
-    instance.product.quantity += instance.quantity
-    instance.product.save()
-
